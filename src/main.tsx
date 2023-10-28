82b921efe48abf0f611bf42bcf3e2df3dc030559
Legrand Jérémie <jlegrand@cxfirst.com>
2023-10-28 22:15:51 +0200

    fix(file): remove unused file

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import router from './routes/Root';
 import { RouterProvider } from 'react-router-dom';
 
